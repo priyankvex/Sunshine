32d1be0a22e33a025b2b68e42aa9003402058813
Priyank Verma <priyank.rip@gmail.com>
2014-12-12 01:12:33 +0530

    Added settings for the units of temprature shown.

--- app/src/main/java/com/example/priyank/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/priyank/sunshine/ForecastFragment.java
@@ -233,6 +233,17 @@ public class ForecastFragment extends Fragment {
          * Prepare the weather high/lows for presentation.
          */
         private String formatHighLows(double high, double low) {
+            //Data is fetched in Celcius by default. Changing the data as per the preferences set by the user
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = prefs.getString(getString(R.string.pref_unit_key), getString(R.string.pref_unit_metric));
+
+            if(unitType.equals(getString(R.string.pref_unit_imperial))){
+                high = (high * 1.8) + 32;
+                low = (low * 1.8) + 32;
+            }
+            else if(!unitType.equals(getString(R.string.pref_unit_metric))){
+                Log.d("ForecastFragment", "Unit type not found");
+            }
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
